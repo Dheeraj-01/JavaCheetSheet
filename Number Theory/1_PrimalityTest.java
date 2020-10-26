d8bfbf8b0a278fa061c90bfa7274f9656af953af
Dheeraj Bhagchandani <54698983+Dheeraj-01@users.noreply.github.com>
2020-10-26 17:34:04 +0530

    Create 1_PrimalityTest.java

---
+++ Number Theory/1_PrimalityTest.java
@@ -0,0 +1,16 @@
+       
+       
+       private static boolean isPrime(int n) {
+
+            if(n == 1)
+              return false;
+            for(int i=2; i*i <= n; i++) {
+              if(n%i == 0)
+                return false;
+            }
+            return true;
+
+          }
+          
+          
+          
