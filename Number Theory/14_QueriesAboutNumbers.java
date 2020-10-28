4768d7bbecda24828a3c36debab4b293febcf397
Dheeraj Bhagchandani <54698983+Dheeraj-01@users.noreply.github.com>
2020-10-29 00:33:14 +0530

    Update 14_QueriesAboutNumbers.java

--- Number Theory/14_QueriesAboutNumbers.java
+++ Number Theory/14_QueriesAboutNumbers.java
@@ -18,8 +18,44 @@ Therefore you compromise with him and decided that everyone must do his/her own 
 */
 
 
-static HashMap<Long, Integer> map = new HashMap<Long, Integer>();
-	private static int total = 1;
+	static HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+	private static long total = 1;
+	private static long n;
+
+
+	static long getDivisorDivisible_byK(long k) {
+        if (n % k != 0) return 0;
+        k = n / k;
+        long res = 1;
+        for (Map.Entry<Long, Integer> i: map.entrySet()) {
+            long key = i.getKey();
+            int val = i.getValue();
+            int cnt = 0;
+            while (k % key == 0) {
+                k /= key; cnt++;
+            }
+            res *= (cnt + 1);
+        }
+        return res;
+    }
+
+
+    static long getCommonDivisor(long k) {
+        long commonDivisor = 1;
+        for (Map.Entry<Long, Integer> i: map.entrySet()) {
+            long key = i.getKey();
+            if (k % key == 0) {
+                int cnt = 0;
+                while (k % key == 0) {
+                    k /= key; cnt++;
+                }
+                commonDivisor *= (Math.min(i.getValue(), cnt)+1);
+            }
+        }
+        return commonDivisor;
+    }
+
+
 	 
 	 private static void primefact(long n) {
 
@@ -48,73 +84,33 @@ static HashMap<Long, Integer> map = new HashMap<Long, Integer>();
 	 
 	 public static void main(String[] args) {
 		
-		 Scanner sc = new Scanner(System.in);
-		 long n = sc.nextLong();
+		 FastScanner sc = new FastScanner();
+		  n = sc.nextLong();
+		 StringBuilder builder = new StringBuilder();
 		 int q = sc.nextInt();
+		 PrintWriter out = new PrintWriter(System.out);
 		 primefact(n);
 		 while(q-- != 0) {
 			 int t = sc.nextInt();
 			 long k = sc.nextLong();
 			 if(t == 1) {
-				 	long res = 1;
-					for(long p : map.keySet())
-					{
-						long cnt = 0;
-						while(k % p == 0) {
-							cnt++;
-							k /= p;
-						}
-						
-						res *= (Math.min(cnt , map.get(p)) + 1);
-					}
+				 	long res = getCommonDivisor(k);
+					 builder.append(res+"\n");
 					
-					System.out.println(res);
 			 }
 			 else if(t == 2) {
-				 	long res = 1;
-					for(long p : map.keySet())
-					{
-						long cnt = 0;
-						while(k % p == 0) {
-							cnt++;
-							k /= p;
-						}
-						if(cnt > map.get(p))
-						{
-							res = 0;
-							break;
-						}
-						res *= (map.get(p) - cnt + 1);
-					}
-					if(k > 1) res = 0;
-					System.out.println(res);
+				 long res = getDivisorDivisible_byK(k);
+				 builder.append(res+"\n");
 			 
 			 }
 			 else {
 				 
-				 	long res = 1;
-					for(long p : map.keySet())
-					{
-						long cnt = 0;
-						while(k % p == 0) {
-							cnt++;
-							k /= p;
-						}
-						if(cnt > map.get(p))
-						{
-							res = 0;
-							break;
-						}
-						res *= (map.get(p) - cnt + 1);
-					}
-					if(k > 1) res = 0;
-					System.out.println(total - res);
-				 
+				 long res = total - getDivisorDivisible_byK(k);
+				 builder.append(res+"\n");
 			 }
 			 
 
 		 }
+		 out.print(builder);
+		 out.flush();
 	}
-	 
-	 
-	 
