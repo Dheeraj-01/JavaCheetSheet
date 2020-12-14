3c5faab44bdf92f655c5f688f93efcda78d7d917
Dheeraj Bhagchandani <54698983+Dheeraj-01@users.noreply.github.com>
2020-12-15 00:46:32 +0530

    Create 1_Connecting_Components.java

---
+++ Graph/Graph1/1_Connecting_Components.java
@@ -0,0 +1,21 @@
+// Connecting cc
+private static int Connecting_Components() {
+		int cc = 0;
+        for(int i=1; i<=n; i++) {
+        	if(vis[i] == 0) {
+        		dfs(i);
+        		cc++;
+        	}
+        }
+		
+		return cc;
+	}
+
+	private static void dfs(int node) {
+		vis[node] = 1;
+		for(int child : adj[node]) {
+			if(vis[child] == 0)
+				dfs(child);
+		}
+    }
+	
