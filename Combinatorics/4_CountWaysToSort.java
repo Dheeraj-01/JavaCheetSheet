d9b434e019041e25c8a96da6318ed6d37086fa85
Dheeraj Bhagchandani <54698983+Dheeraj-01@users.noreply.github.com>
2021-01-08 01:22:51 +0530

    Create 4_CountWaysToSort.java

---
+++ Combinatorics/4_CountWaysToSort.java
@@ -0,0 +1,36 @@
+
+
+
+static long countWaysToSort(ArrayList<Pair> arr) {
+		ArrayList<Pair> a = new ArrayList<Pair>(arr);
+		Collections.sort(a);
+		long ways = 1;
+		long prevEqual=1;
+		for (int i=1; i<a.size(); i++) {
+			if (a.get(i).x == a.get(i-1).x && a.get(i).y == a.get(i-1).y)
+				prevEqual = (prevEqual+1)%mod;
+			else
+				prevEqual=1;
+			ways=(ways*(prevEqual)%mod)%mod;
+			if (a.get(i).x<a.get(i-1).x || a.get(i).y<a.get(i-1).y)
+				ways=(0);
+		}
+		return ways%mod;
+	}
+  
+  
+  
+  static class Pair implements Comparable<Pair> {
+		int x, y;
+
+		Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Pair o) {
+			if(x == o.x)return Integer.compare(y, o.y);
+			return Integer.compare(this.x, o.x);
+		}
+	}
