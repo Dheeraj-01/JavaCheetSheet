d522065dccd1573f0c9de93bc21b92b2c2b0dbef
Dheeraj Bhagchandani <54698983+Dheeraj-01@users.noreply.github.com>
2021-01-13 01:21:33 +0530

    Update 2_sieve_of_Eratosthenes.java

--- Number Theory/2_sieve_of_Eratosthenes.java
+++ Number Theory/2_sieve_of_Eratosthenes.java
@@ -1,25 +1,22 @@
 
 
-public static int[] generatePrimes(int n) {
-	    boolean[] prime = new boolean[n + 1];
-	    Arrays.fill(prime, 2, n + 1, true);
-	    for (int i = 2; i * i <= n; i++)
-	      if (prime[i])
-	        for (int j = i * i; j <= n; j += i)
-	          prime[j] = false;
-	    int[] primes = new int[n + 1];
-	    int cnt = 0;
-	    for (int i = 0; i < prime.length; i++)
-	      if (prime[i])
-	        primes[cnt++] = i;
-
-	    return Arrays.copyOf(primes, cnt);
-	  }
+public static boolean[] generatePrimes(int n) {
+	boolean[] prime = new boolean[n+1];
+        Arrays.fill(prime, true);
+        for(int i = 2; i < n+1; ++i)
+            if(prime[i]) {
+                if(1l * i * i < n+1)
+                    for(int j = i * i; j < n+1; j += i) {
+                        prime[j] = false;
+                    }
+            }
+        return prime;
+	}
     
     
     public static void main(String[] args) throws IOException {
-		int arr[] = generatePrimes(89000000);
-		System.out.println(arr.length);
+		boolean[] prime = generatePrimes(n);
+		System.out.println(prime.length);
 	}
     
     
