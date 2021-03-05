9e970e776a9b7e9e317b1a6aa8af4898af5c9846
Dheeraj Bhagchandani <54698983+Dheeraj-01@users.noreply.github.com>
2021-03-05 18:53:02 +0530

    Update dsu3.java

--- DSU/dsu3.java
+++ DSU/dsu3.java
@@ -1,6 +1,16 @@
-static int N = 200005;
+
+	static int N = 200005;
 	static int parent[] = new int[N];
 	static int get(int a){
 		if(a==parent[a]) return a;
 		else return (get(parent[a]));
 	}
+
+public static void process() throws IOException {
+	
+		for(int i=1;i<N;i++)
+	    {
+	        parent[i]=i;
+	    }
+	
+}
